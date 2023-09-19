0fc306e25121f53762d4a0b92f9a0ffa88403321
el898 <ebubeexcel3@gmail.com>
2023-09-19 19:20:18 +0100

    functions

--- external.js
+++ external.js
@@ -1,9 +1,28 @@
-function getComputerChoice(){
-    var Choices =  ["Rock","Paper","Scissors"];
-    var randomReturn =    [Math.floor(Math.random()*Choices.length)]
-    var Choices = Choices[randomReturn];
-    console.log(Choices);
-    
+var Choices = ["rock", "paper", "scissors"];
+var playerScore, computerScore;
+
+function getComputerChoice() {
+    var randomIndex = Math.floor(Math.random() * Choices.length);
+    var computerChoice = Choices[randomIndex];
+    console.log("Computer chose: " + computerChoice);
+    return computerChoice;
 }
 
-getComputerChoice();
+function playerChoice() {
+    var selectChoice = prompt("Your Turn: " + Choices.join(', ')).toLowerCase();
+    
+    while (!Choices.includes(selectChoice)) {
+        console.log(selectChoice + " is not allowed.");
+        selectChoice = prompt("Your Turn: " + Choices.join(', ')).toLowerCase();
+    }
+
+    return selectChoice;
+}
+
+function playGame(){
+  playerSelection = playerChoice();
+  computerSelection = getComputerChoice();
+  if (playerSelection == computerSelection){
+    console.log("A Tie!");
+  }
+}
